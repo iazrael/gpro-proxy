072875236c63a7430ba305138e9724a2863e548b
azraellong <azraellong@zego.im>
2024-04-01 19:51:31 +0800

    add start

--- app.js
+++ app.js
@@ -35,10 +35,8 @@ app.use('/', createProxyMiddleware({
     }
 }));
 
-// // 启动服务器
-// const port = 3000;
-// app.listen(port, () => {
-//     console.log(`Proxy server is running on port ${port}`);
-// });
-
-module.exports = app
+// 启动服务器
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Proxy server is running on port ${port}`);
+});
