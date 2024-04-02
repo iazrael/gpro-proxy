5dcb3eb76c83bbffa9e4ce362c85ed148b32e355
azraellong <azraellong@zego.im>
2024-04-02 10:54:36 +0800

    feat: local proxy 可配

--- app.js
+++ app.js
@@ -2,43 +2,39 @@ const express = require('express')
 const {
     createProxyMiddleware
 } = require('http-proxy-middleware');
+
+const config = require('./config')
+
 const app = express()
 
-// const HttpsProxyAgent = require('https-proxy-agent');
 
-const proxyDomain = 'generativelanguage.googleapis.com'
 
 app.get('/', (req, res) => {
     res.send('Hello Kitty!')
 })
 
-// // 代理服务器的地址
-// const proxyHost = '127.0.0.1';
-// const proxyPort = 7890;
-// // 设置代理
-// const proxyUrl = `http://${proxyHost}:${proxyPort}`;
-// const agent = new HttpsProxyAgent(proxyUrl);
+
 
 app.use('/', createProxyMiddleware({
-    target: `https://${proxyDomain}`,
+    target: `https://${config.PROXY_TARGET_DOMAIN}`,
     changeOrigin: true,
     secure: false,
-    // agent: agent,
+    agent: config.LOCAL_PROXY_AGENT,
     onProxyReq: (proxyReq, req, res) => {
-        console.log(`proxy for ${req.url}`)
+        console.log(`[*] proxy for ${req.url}`)
         // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
         // proxyReq.headers['x-forwarded-for'] = '';
         // proxyReq.headers['x-real-ip'] = '';
     },
     onProxyRes: (proxyRes, req, res) => {
-        console.log(`proxy from ${req.url} back`)
+        console.log(`[*] proxy from ${req.url} back`)
         proxyRes.headers['Access-Control-Allow-Origin'] = '*';
         proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, PATCH, DELETE';
         proxyRes.headers['Access-Control-Allow-Headers'] = 'X-Requested-With,content-type';
         proxyRes.headers['Access-Control-Allow-Credentials'] = true;
     },
     onError: (err, req, res, target) => {
-        console.log(`proxy get error for ${req.url} with target ${target}`)
+        console.error(`[!] proxy get error for ${req.url} with target ${target}, error: ${err}`)
         res.writeHead(500, {
             'Content-Type': 'text/plain',
         });
---
+++ config.js
@@ -0,0 +1,19 @@
+
+let agent = null
+if (process.env.LOCAL_PROXY) {
+    console.log('[*] LOCAL_PROXY enable')
+    const HttpsProxyAgent = require('https-proxy-agent');
+    // 代理服务器的地址
+    const proxyHost = '127.0.0.1';
+    const proxyPort = 7890;
+    // 设置代理
+    const proxyUrl = `http://${proxyHost}:${proxyPort}`;
+    agent = new HttpsProxyAgent(proxyUrl);
+}
+
+
+
+module.exports = {
+    PROXY_TARGET_DOMAIN: "generativelanguage.googleapis.com",
+    LOCAL_PROXY_AGENT: agent,
+}
